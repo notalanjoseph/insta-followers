7a58fc7951d528c026784ad5bff6d2b1c2d3383b
Alan Joseph <90091512+notalanjoseph@users.noreply.github.com>
2023-05-08 21:25:23 +0530

    Update insta.py

--- insta.py
+++ insta.py
@@ -6,68 +6,80 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 count = 0
-username = 'not_a_l_a_n'
-password = ''
+iter = 0
+username = ""
+password = ""
+
 
 def login(driver):
     driver.find_element("name", "username").send_keys(username)
     driver.find_element("name", "password").send_keys(password)
-    driver.find_element("name", "password").send_keys(u'\ue007')
-    
+    driver.find_element("name", "password").send_keys("\ue007")#
+
+
 def click_button_with_css(driver, css_selector):
     element = WebDriverWait(driver, 20).until(
-        EC.element_to_be_clickable((By.CSS_SELECTOR, css_selector)))
+        EC.element_to_be_clickable((By.CSS_SELECTOR, css_selector))
+    )
     element.click()
-    
-def navigate_to_followers(driver):
+
+
+def navigate_to_profile(driver):
     dropdown_css = '[alt*="' + username + '"]'
-    profile_css = "[href*=\"" + username + "\"]"
+    profile_css = '[href*="' + username + '"]'#
     click_button_with_css(driver, dropdown_css)
     click_button_with_css(driver, profile_css)
-    
-    
-def get_usernames_from_dialog(driver):
-    list_xpath ="//div[@role='dialog']//li"
-    
-    
- #   try:
-    WebDriverWait(driver, 20).until(
-            #EC.presence_of_element_located((By.XPATH, list_xpath)))
-       driver.find_element(By.XPATH, list_xpath)) #SO
-#    except:
-#        print("wait error")
-    
-    try:
-        scroll_down(driver)
-    except:
-        print("scroll error")
-    
-    try:
-        list_elems = driver.find_element("xpath", list_xpath)
-    except:
-        print("list error")
-    
-    users = []
-    for i in range(len(list_elems)):
-        try:
-            row_text = list_elems[i].text
-            if "Follow" in row_text or "Remove" in row_text:
-                username = row_text[:row_text.index("\n")]
-                users += [username]
-        except:
-            print("continue")
-            
-    return users     
 
-            
+
+def get_usernames_from_dialog(driver):
+    list_xpath = "//div[@role='dialog']//li"#
+    xpath_name = '//div[@class="_aano"]/*[1]'
+    list_elems = []
+    
+    time.sleep(4)
+    
+    scroll_down(driver)
+    time.sleep(2)
+    scroll_down(driver)
+    time.sleep(2)
+    scroll_down(driver)
+    time.sleep(2)
+    scroll_down(driver)
+    time.sleep(2)
+    scroll_down(driver)
+    time.sleep(2)
+    scroll_down(driver)
+    #add more if needed
+
+    # ancestor div that contains the names is the first child of div with class _aano
+    parent_div = driver.find_element("xpath", xpath_name)
+
+    # get all the child div elements of the parent div into a list
+    child_divs = parent_div.find_elements("xpath", ".//div")
+
+    # iterate through the child divs to extract the names
+    for div in child_divs:
+        name = div.text
+        name = name.replace("\nVerified", "")
+        names_to_ignore = ["", "·", "Follow", "Remove", "Verified", "Following"]
+        if "\n" not in name:
+            if name not in names_to_ignore:
+                list_elems.append(name)
+
+    list_elems = list(set(list_elems))
+
+    return list_elems
+
+
 def scroll_down(driver):
     global count
-    iter = 0
+    global iter#
     while 1:
         scroll_top_num = str(iter * 1000)
         iter +=1
-        driver.execute_script("document.querySelector('div[role=dialog] ul').parentNode.scrollTop=" + scroll_top_num)
-        
+        driver.execute_script(
+            "document.querySelector('div[class=_aano]').scrollTop=" + scroll_top_num
+        )
         try:
             WebDriverWait(driver, 1).until(check_difference_in_count)
         except:
@@ -75,68 +87,57 @@ def scroll_down(driver):
             break
     return
 
+
 def check_difference_in_count(driver):
     global count
-    new_count = len(driver.find_element("xpath", "//div[@role='dialog']//li"))
+    new_count = len(driver.find_elements("xpath", "//div[@role='dialog']"))
     if count != new_count:
         count = new_count
         return True
     else:
         return False
-    
-    
-def no_followback(followers, following):
-    followers.sort()
-    following.sort()
-    no_followback_list = []
-    for i in range(len(following)):
-        try:
-            followers.index(following[i])
-        except ValueError:
-            no_followback_list += [following[i]]
-    return no_followback_list
 
 
 
 def __main__():
-#    driver = webdriver.Edge(EdgeChromiumDriverManager().install())
+    #    driver = webdriver.Edge(EdgeChromiumDriverManager().install())
     driver = webdriver.Edge()
-    driver.get('https://www.instagram.com/accounts/login')
+    driver.get("https://www.instagram.com/accounts/login")
     time.sleep(2)
-    
+
     login(driver)
-    navigate_to_followers(driver)
-    
-    
-    followers_css = "[href*=\"" + username + "/followers/\"]"
+    time.sleep(5)
+    navigate_to_profile(driver)
+    time.sleep(2)
+
+    followers_css = '[href*="' + username + '/followers/"]'#
     css_select_close = '[aria-label="Close"]'
-    following_css = "[href*=\"" + username + "/following/\"]"
-    
+    following_css = '[href*="' + username + '/following/"]'#
+
     click_button_with_css(driver, followers_css)
     followers_list = get_usernames_from_dialog(driver)
+    print("---------------------------------------")
+    print("FOLLOWERS LIST: ", len(followers_list))
+    print(followers_list)
     
+    time.sleep(2)
     click_button_with_css(driver, css_select_close)
     time.sleep(2)
-    
+
     click_button_with_css(driver, following_css)
     following_list = get_usernames_from_dialog(driver)
+    print("---------------------------------------")
+    print("FOLLOWING LIST: ", len(following_list))
+    print(following_list)
     
-    
-    no_followbacks = no_followback(followers_list, following_list)
-    for i in range(len(no_followbacks)):
-        print (no_followbacks[i])
-    
-    
-    
-    
-    
-    time.sleep(100)
-    
-    
-    driver.quit()
-    
-    
+    print("---------------------------------------")
+    print("PEOPLE YOU FOLLOW BUT DO NOT FOLLOW YOU BACK:")
+    for name in following_list:
+        if name not in followers_list:
+            print(name)
 
-  
-    
-__main__() 
+    time.sleep(20)
+    driver.quit()
+
+
+__main__()
