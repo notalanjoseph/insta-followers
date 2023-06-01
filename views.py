1939724122e4faa7652dff6833e52d6db7dfef38
Alan Joseph <90091512+notalanjoseph@users.noreply.github.com>
2023-06-01 19:27:49 +0530

    Update views.py

--- views.py
+++ views.py
@@ -10,6 +10,7 @@ myviews = Blueprint(__name__, "views")
 def getData():
     if request.method == 'POST':
         #run code and display output
-        return render_template("index.html", ans = "hello ther")
+        username = request.form["username"]
+        return render_template("index.html", ans = username)
     else:
         return render_template("index.html")
